303fdbf21c790c7521fdc701fb59d759a6745c68
LoickDev97438 <loickcherimont@gmail.com>
2023-04-05 00:05:54 +0200

    Remove run_option, debug show_contacts, add a default contact to contact_book

--- contact_book.c
+++ contact_book.c
@@ -1,45 +1,71 @@
 #include <stdio.h>
 #include <string.h>
 
+// Define what is a contact
+struct Contact
+{
+    char phone_number[11];
+    char first_name[256];
+    char last_name[256];
+    char address[256];
+    char email[256];
+};
+
+// To use "Contact" instead of "struct Contact"
+typedef struct Contact Contact;
+
 // Function prototypes
 void display_options();
 int get_user_option(int option);
-void run_option(int option);
 void create_contact();
 void remove_contact();
 void modify_contact();
-void show_contacts(struct Contact contact_book);
+void show_contacts(Contact *contact_book);
 
-// Define what is a contact
-struct Contact {
-    char phone_number[11];
-    char first_number[256];
-    char last_number[256];
-    char address[256];
-    char email[256];
-} test_contact, contacts[3];
-
+// ************************** MAIN FUNCTION **************************
 int main(void)
 {
     int target_option;
+    Contact contact_book[10];
 
-    // A sample array of contacts**********************************
-    strcpy(test_contact.phone_number, "0123456789");
-    strcpy(test_contact.first_number, "John");
-    strcpy(test_contact.last_number, "Doe");
-    strcpy(test_contact.address, "11 Test Street, Coding City XXXXX");
-    strcpy(test_contact.email, "johndoe@gmail.com");
-
-    contacts[0] = test_contact;
-    //*********************************************************
+    // Default contact ever in Contact Book
+    strcpy(contact_book[0].phone_number, "0123456789");
+    strcpy(contact_book[0].first_name, "John");
+    strcpy(contact_book[0].last_name, "DOE");
+    strcpy(contact_book[0].address, "37 street of test, Test city 84632, Imaginary Country");
+    strcpy(contact_book[0].email, "john.doe@gmail.com");
 
     display_options();
     target_option = get_user_option(target_option);
-    // run_option(target_option);
-    show_contacts(contacts);
+
+    switch (target_option)
+    {
+    case 1: // TODO
+        create_contact();
+        break;
+
+    case 2: // TODO
+        remove_contact();
+        break;
+
+    case 3: // TODO
+        modify_contact();
+        break;
+
+    case 4: // OK
+        show_contacts(contact_book);
+        break;
+
+    default:
+        // TODO: ERROR HANDLING
+        puts("Sorry, this option is not available!");
+        break;
+    }
 
     return 0;
 }
+
+// ************************** END MAIN FUNCTION **************************
 
 // Functions
 void display_options()
@@ -60,56 +86,38 @@ int get_user_option(int option)
     return option;
 }
 
-void run_option(int option) {
-    switch (option)
-    {
-    case 1:
-        create_contact();
-        break;
-    
-    case 2:
-        remove_contact();
-        break;
-    
-    case 3:
-        modify_contact();
-        break;
-    
-    case 4:
-        // show_contacts();
-        puts("SHOW CONTACTS");
-        break;
-    
-    default:
-    // TODO: ERROR HANDLING
-        puts("Sorry, this option is not available!");
-        break;
-    }
-}
-
 // Options
-void create_contact() 
+void create_contact()
 {
-    puts("Create a contact");
+    printf("***************** CREATE A NEW CONTACT *****************\n");
 }
 
-void remove_contact() 
+void remove_contact()
 {
-    puts("Remove a contact");
+    printf("***************** REMOVE A CONTACT *****************\n");
 }
 
-void modify_contact() 
+void modify_contact()
 {
-    puts("Modify a contact");
+    printf("***************** REMOVE A CONTACT *****************\n");
 }
 
-// To Debug
-void show_contacts(struct Contact contact_book)
+// Show all current contact
+// Recorded in the Contact book
+void show_contacts(Contact *contact_book)
 {
-    size_t total_contacts = sizeof(contact_book)/sizeof(contact_book[0]);
+    int total_contacts = sizeof(*contact_book) / sizeof(contact_book[0]);
 
-    for(int i = 0; i < sizeof(contact_book); i++) 
+    printf("***************** ALL YOUR CONTACTS *****************\n");
+
+    for (int i = 0; i < total_contacts; i++)
     {
-        printf("%d\n", contact_book[i]);
+        printf("==================================================================\n");
+        printf("Phone number: %s\n", contact_book[i].phone_number);
+        printf("First name: %s\n", contact_book[i].first_name);
+        printf("Last name: %s\n", contact_book[i].last_name);
+        printf("Address: %s\n", contact_book[i].address);
+        printf("Email: %s\n", contact_book[i].email);
+        printf("==================================================================\n");
     }
 }
