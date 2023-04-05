9b17c9bce38d105901a5e6d338fe5b716d8557df
LoickDev97438 <loickcherimont@gmail.com>
2023-04-06 00:37:09 +0200

    Switch from Functional to Procedural, setup create contact option

--- contact_book.c
+++ contact_book.c
@@ -14,19 +14,22 @@ struct Contact
 // To use "Contact" instead of "struct Contact"
 typedef struct Contact Contact;
 
+Contact contact_book[10];
+
 // Function prototypes
 void display_options();
 int get_user_option(int option);
-void create_contact();
-void remove_contact();
-void modify_contact();
-void show_contacts(Contact *contact_book);
+// void add_contact_to(Contact *contact_book, int counter);
+// void remove_contact();
+// void modify_contact();
+// void show_contacts(Contact *contact_book);
+// size_t get_current_size(Contact *contact_book);
 
 // ************************** MAIN FUNCTION **************************
 int main(void)
 {
     int target_option;
-    Contact contact_book[10];
+    int contact_counter = 1;
 
     // Default contact ever in Contact Book
     strcpy(contact_book[0].phone_number, "0123456789");
@@ -41,19 +44,85 @@ int main(void)
     switch (target_option)
     {
     case 1: // TODO
-        create_contact();
+        // Add a new contact using user inputs
+        char new_phone_number[11];
+        char new_first_name[256];
+        char new_last_name[256];
+        char new_address[256];
+        char new_email[256];
+
+        printf("***************** CREATE A NEW CONTACT *****************\n\n");
+
+        printf("Enter the firstname: ");
+        scanf("%s", &new_first_name);
+        strcpy(contact_book[contact_counter].first_name, new_first_name);
+
+        printf("Enter the lastname: ");
+        scanf("%s", &new_last_name);
+        strcpy(contact_book[contact_counter].last_name, new_last_name);
+
+        printf("Enter the phone number: ");
+        scanf("%s", &new_phone_number);
+        strcpy(contact_book[contact_counter].phone_number, new_phone_number);
+
+        printf("Enter the address: ");
+        scanf("%s", &new_address);
+        strcpy(contact_book[contact_counter].address, new_address);
+
+        printf("Enter the email: ");
+        scanf("%s", &new_address);
+        strcpy(contact_book[contact_counter].email, new_email);
+
+        contact_counter++;
+
+        printf("\nNEW CONTACT ADDED!\n");
+        printf("CONTACTS: %d", contact_counter);
+
+        // Display all contacts
+        size_t current_size = sizeof(contact_book) / sizeof(Contact);
+
+        printf("***************** ALL YOUR CONTACTS *****************\n");
+
+        printf("\nYOU HAVE ACTUALLY %d CONTACTS\n", current_size);
+
+        for (int i = 0; i < current_size; i++)
+        {
+            printf("==================================================================\n");
+            printf("Phone number: %s\n", contact_book[i].phone_number);
+            printf("First name: %s\n", contact_book[i].first_name);
+            printf("Last name: %s\n", contact_book[i].last_name);
+            printf("Address: %s\n", contact_book[i].address);
+            printf("Email: %s\n", contact_book[i].email);
+            printf("==================================================================\n");
+        }
         break;
 
     case 2: // TODO
-        remove_contact();
+        puts("***************** REMOVE A CONTACT *****************\n");
         break;
 
     case 3: // TODO
-        modify_contact();
+        puts("***************** MODIFY A CONTACT *****************\n");
         break;
 
     case 4: // OK
-        show_contacts(contact_book);
+        // Display all contacts
+        current_size = sizeof(contact_book) / sizeof(Contact);
+
+        printf("***************** ALL YOUR CONTACTS *****************\n");
+
+        printf("\nYOU HAVE ACTUALLY %d CONTACTS\n", current_size);
+
+        for (int i = 0; i < current_size; i++)
+        {
+            printf("==================================================================\n");
+            printf("Phone number: %s\n", contact_book[i].phone_number);
+            printf("First name: %s\n", contact_book[i].first_name);
+            printf("Last name: %s\n", contact_book[i].last_name);
+            printf("Address: %s\n", contact_book[i].address);
+            printf("Email: %s\n", contact_book[i].email);
+            printf("==================================================================\n");
+        }
         break;
 
     default:
@@ -87,37 +156,80 @@ int get_user_option(int option)
 }
 
 // Options
-void create_contact()
-{
-    printf("***************** CREATE A NEW CONTACT *****************\n");
-}
+// void add_contact_to(Contact *contact_book, int counter)
+// {
+//     char new_phone_number[11];
+//     char new_first_name[256];
+//     char new_last_name[256];
+//     char new_address[256];
+//     char new_email[256];
 
-void remove_contact()
-{
-    printf("***************** REMOVE A CONTACT *****************\n");
-}
+//     // TODO : Debug
+//     int last_index = counter;
 
-void modify_contact()
-{
-    printf("***************** REMOVE A CONTACT *****************\n");
-}
+//     printf("***************** CREATE A NEW CONTACT *****************\n\n");
+
+//     printf("Enter the firstname: ");
+//     scanf("%s", &new_first_name);
+//     strcpy(contact_book[last_index].first_name, new_first_name);
+
+//     printf("Enter the lastname: ");
+//     scanf("%s", &new_last_name);
+//     strcpy(contact_book[last_index].last_name, new_last_name);
+
+//     printf("Enter the phone number: ");
+//     scanf("%s", &new_phone_number);
+//     strcpy(contact_book[last_index].phone_number, new_phone_number);
+
+//     printf("Enter the address: ");
+//     scanf("%s", &new_address);
+//     strcpy(contact_book[last_index].address, new_address);
+
+//     printf("Enter the email: ");
+//     scanf("%s", &new_address);
+//     strcpy(contact_book[last_index].email, new_email);
+
+//     counter++;
+
+//     printf("\nNEW CONTACT ADDED!\n");
+//     printf("CONTACTS: %d", counter);
+// }
+
+// void remove_contact()
+// {
+//     printf();
+// }
+
+// void modify_contact()
+// {
+//     printf("***************** REMOVE A CONTACT *****************\n");
+// }
 
 // Show all current contact
 // Recorded in the Contact book
-void show_contacts(Contact *contact_book)
-{
-    int total_contacts = sizeof(*contact_book) / sizeof(contact_book[0]);
+// void show_contacts(Contact *contact_book)
+// {
+//     size_t current_size = get_current_size(contact_book);
 
-    printf("***************** ALL YOUR CONTACTS *****************\n");
+//     printf("***************** ALL YOUR CONTACTS *****************\n");
 
-    for (int i = 0; i < total_contacts; i++)
-    {
-        printf("==================================================================\n");
-        printf("Phone number: %s\n", contact_book[i].phone_number);
-        printf("First name: %s\n", contact_book[i].first_name);
-        printf("Last name: %s\n", contact_book[i].last_name);
-        printf("Address: %s\n", contact_book[i].address);
-        printf("Email: %s\n", contact_book[i].email);
-        printf("==================================================================\n");
-    }
-}
+//     printf("\nYOU HAVE ACTUALLY %d CONTACTS\n", current_size);
+
+//     for (int i = 0; i < current_size; i++)
+//     {
+//         printf("==================================================================\n");
+//         printf("Phone number: %s\n", contact_book[i].phone_number);
+//         printf("First name: %s\n", contact_book[i].first_name);
+//         printf("Last name: %s\n", contact_book[i].last_name);
+//         printf("Address: %s\n", contact_book[i].address);
+//         printf("Email: %s\n", contact_book[i].email);
+//         printf("==================================================================\n");
+//     }
+// }
+
+// size_t get_current_size(Contact *contact_book)
+// {
+//     printf("sizeof: %d", sizeof(*contact_book) / sizeof(Contact));
+
+//     return sizeof(*contact_book) / sizeof(Contact);
+// }
